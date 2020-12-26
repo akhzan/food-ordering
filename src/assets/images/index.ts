789a98cd2f3115d77d5a22830f33f44bb678b347
Syafrizal <rizal@payfazz.com>
2020-12-26 23:10:44 +0700

    move asset images to public/images

--- src/assets/images/index.ts
+++
@@ -1,7 +0,0 @@
-import burgerIcon from './burger(24x24)@3x.png'
-import bowlIcon from './noodles-2(24x24)@3x.png'
-import fruitsIcon from './fruits(24x24)@3x.png'
-import cakeIcon from './slice-cake(24x24)@3x.png'
-import drinkIcon from './soft-drink(24x24)@3x.png'
-
-export { burgerIcon, bowlIcon, fruitsIcon, cakeIcon, drinkIcon }
